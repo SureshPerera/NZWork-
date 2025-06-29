600bfe3867de12202a63ade2ec1d90d3ab5c27e4
SureshPerera <avishkaruchirasuresh1@gmail.com>
2025-06-29 16:14:36 +0530

    update dependancy injection for sql server

---
+++ NZWork.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWork.API.Model.Domain;
+
+namespace NZWork.API.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+        public NZWalksDbContext(DbContextOptions dbContextOptions): base(dbContextOptions) 
+        {
+            
+        }
+        public DbSet<Difficulty> Difficulties{ get; set; }
+        public DbSet<Region> Regions{ get; set; }
+        public DbSet<Work> Works{ get; set; }
+    }
+}
